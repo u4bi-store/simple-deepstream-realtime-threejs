3e0fa91ef27270a6c6737aa52c7d7879431afcac
Yu Myungjae <u4bi@letsee.io>
2017-07-21 14:13:19 +0900

    delete test consolelog

--- index.js
+++ index.js
@@ -22,11 +22,14 @@ function init(){
 
     scene.add(new THREE.AxisHelper(3));
 
+
+
     // plane
     var plane = new THREE.Mesh(new THREE.PlaneGeometry(6, 6), new THREE.MeshPhongMaterial({color: 0xFFFFFF}));
     scene.add(plane);
     plane.rotateX(-Math.PI/2);
     //
+
 
 
     // light
@@ -36,8 +39,10 @@ function init(){
     //
 
     
+
     mesh = new THREE.Mesh( new THREE.BoxGeometry(.3, .3, .3), new THREE.MeshBasicMaterial( { color: 0x00FFFF, wireframe : true } ));
     console.log(mesh);
+
 
 
     // deepstream connection
@@ -50,18 +55,9 @@ function init(){
 
         items.whenReady(function(e){
 
-            // TEST ADD FUNCTION
-            // add('선물', {
-            //     x : 0,
-            //     y : 0,
-            //     z : 0
-            // });
-
             e.getEntries().forEach(function(entry){
                 console.log(entry);
 
-                // TEST REMOVE FUNCTION
-                // remove(entry);
 
                 // get entry record
                 client.record.getRecord(entry).whenReady(function(e){
@@ -84,14 +80,10 @@ function init(){
 
         // dynamic record add event
         items.on('entry-added', function(recordName, index){
-            console.log('add ', recordName, index);
+            // console.log('add ', recordName, index);
 
             client.record.getRecord(recordName).whenReady(function(e){
-                /** console test
-                    
-                    add('선물', { x : 0, y : 5, z : 0 })
 
-                */
                 // create mesh box
                 var 
                     box = create(recordName, e.get());
@@ -106,7 +98,7 @@ function init(){
 
         // dynamic record remove event
         items.on('entry-removed', function(recordName, index){
-            console.log('remove ', recordName, index);
+            // console.log('remove ', recordName, index);
             
             cube.forEach(function(e){
                 if( e.name === recordName){
@@ -131,21 +123,10 @@ function init(){
     window.addEventListener('resize', resize);
     render();
 
-
-    // TEST MESH
-    // var box = create('TEST-UUID123', {
-    //     position : {
-    //         x : 0,
-    //         y : 0,
-    //         z : 0
-    //     }
-    // });
-    
-    // scene.add(box);
-    // cube.push(box);
-    //
-
 }
+
+
+
 
 
 function resize(){ 
@@ -156,12 +137,14 @@ function resize(){
 }
 
 
+
+
+
 function render(){
     requestAnimationFrame(render);
     renderer.render(scene, camera);
 
 }
-
 
 
 
@@ -176,6 +159,7 @@ function create(name, data){
     object.name = name;
     return object;
 }
+
 
 
 
@@ -200,6 +184,7 @@ function add(content, position){
 
 
 
+
 // deepstream record remove
 function remove(content){
     items.removeEntry(content);
@@ -209,7 +194,8 @@ function remove(content){
 
 
 
-/* view button */
+
+/* add button */
 function addBox(){
 
     var rand = function(){
@@ -220,11 +206,20 @@ function addBox(){
 
 }
 
+
+
+
+/* remove button */
 function removeBox(){
     remove(cube[cube.length-1].name);
     
 }
 
+
+
+
+
+/* all remove button */
 function allRemove(){
     alert('allRemove');
 
